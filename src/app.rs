3b1c0d24cc583300ee30658c3c90ec2f671a4663
ViniciosLugli <vinicioslugli@gmail.com>
2024-09-07 01:38:54 -0300

    feat(leptos): :sparkles: Fix some issues with packages and build with cargo-leptos. Added charming components for charts

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,5 @@
+use charming::{component::Axis, df, series::Candlestick, Chart, WasmRenderer};
+
 use leptos::*;
 use leptos_meta::*;
 use leptos_router::*;
@@ -8,22 +10,47 @@ pub fn App() -> impl IntoView {
 	provide_meta_context();
 
 	view! {
-		// injects a stylesheet into the document <head>
-		// id=leptos means cargo-leptos will hot-reload this stylesheet
-		<Stylesheet id="leptos" href="/pkg/tailwind.css"/>
-
+		<Stylesheet id="leptos" href="/pkg/ferris-advisor.css" />
+		<script src="https://cdn.jsdelivr.net/npm/echarts@5.4.2/dist/echarts.min.js"></script>
+		<script src="https://cdn.jsdelivr.net/npm/echarts-gl@2.0.9/dist/echarts-gl.min.js"></script>
 		// sets the document title
-		<Title text="Welcome to Leptos"/>
+		<Title text="Welcome to Leptos" />
 
 		// content for this welcome page
 		<Router>
 			<main>
 				<Routes>
-					<Route path="" view=HomePage/>
-					<Route path="/*any" view=NotFound/>
+					<Route path="/" view=HomePage ssr=SsrMode::Async />
+					<Route path="/*any" view=NotFound />
 				</Routes>
 			</main>
 		</Router>
+	}
+}
+
+#[component]
+pub fn GraphExample() -> impl IntoView {
+	let action = create_local_resource(
+		|| (),
+		|_| async move {
+			let chart = Chart::new()
+				.x_axis(Axis::new().data(vec!["2017-10-24", "2017-10-25", "2017-10-26", "2017-10-27"]))
+				.y_axis(Axis::new())
+				.series(Candlestick::new().data(df![
+					[20, 34, 10, 38],
+					[40, 35, 30, 50],
+					[31, 38, 33, 44],
+					[38, 15, 5, 42]
+				]));
+			let renderer = WasmRenderer::new(600, 400);
+			renderer.render("chart", &chart).unwrap();
+		},
+	);
+
+	view! {
+		<div>
+			<div id="chart"></div>
+		</div>
 	}
 }
 
@@ -35,19 +62,21 @@ fn HomePage() -> impl IntoView {
 	view! {
 		<main class="my-0 mx-auto max-w-3xl text-center">
 			<h2 class="p-6 text-4xl">"Welcome to Leptos with Tailwind"</h2>
-			<p class="px-10 pb-10 text-left">"Tailwind will scan your Rust files for Tailwind class names and compile them into a CSS file."</p>
+			<p class="px-10 pb-10 text-left">
+				"Tailwind will scan your Rust files for Tailwind class names and compile them into a CSS file."
+			</p>
 			<button
-				class="bg-amber-600 hover:bg-sky-700 px-5 py-3 text-white rounded-lg"
+				class="py-3 px-5 text-white bg-amber-600 rounded-lg hover:bg-sky-700"
 				on:click=move |_| set_count.update(|count| *count += 1)
 			>
-				"Something's here | "
-				{move || if count.get() == 0 {
-					"Click me!".to_string()
-				} else {
-					count.get().to_string()
+				{move || {
+					if count.get() == 0 { "Click me!".to_string() } else { count.get().to_string() }
 				}}
-				" | Some more text"
 			</button>
+
+			<div class="mt-10">
+				<GraphExample />
+			</div>
 		</main>
 	}
 }
@@ -69,7 +98,5 @@ fn NotFound() -> impl IntoView {
 		resp.set_status(actix_web::http::StatusCode::NOT_FOUND);
 	}
 
-	view! {
-		<h1>"Not Found"</h1>
-	}
+	view! { <h1>"Not Found"</h1> }
 }
