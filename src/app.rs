3564c6aa5a6295f8d0189f24b1838db7d1a39682
ViniciosLugli <vinicioslugli@gmail.com>
2024-09-28 22:52:46 -0300

    fix(dashboard): :art: Fix miss spelling on async load script 🤡

--- src/app.rs
+++ src/app.rs
@@ -10,7 +10,7 @@ pub fn App() -> impl IntoView {
 	view! {
 		<Stylesheet id="leptos" href="/pkg/ferris-advisor.css" />
 		<script src="https://cdn.jsdelivr.net/npm/echarts@5.4.2/dist/echarts.min.js" async></script>
-		<script src="https://cdn.jsdelivr.net/npm/echarts-gl@2.0.9/dist/echarts-gl.min.js async"></script>
+		<script src="https://cdn.jsdelivr.net/npm/echarts-gl@2.0.9/dist/echarts-gl.min.js" async></script>
 		<Title text="Ferris Advisor" />
 
 		<Router>
